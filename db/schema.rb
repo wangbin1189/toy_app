4bb25c82e621aaf0523f464d6154f060a5cafee9
wangbin1189 <wangbin1189@gmail.com>
2018-10-04 09:57:32 -0500

    list6.14

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_172134) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_10_04_142522) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
